767ea601cb94a1248a4f17b2ff431f48205d1adc
tsubaki-8192 <75125080+tsubaki-8192@users.noreply.github.com>
2020-12-13 12:28:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -25,11 +25,14 @@ class Vec2 {
 
 const SCREEN_WIDTH = 640;
 const SCREEN_HEIGHT = 480;
+const TIMEBAR_WIDTH = 360;
 const TILE_SIZE = 32;
 const NUM_TILE_X = SCREEN_WIDTH / TILE_SIZE;
 const NUM_TILE_Y = SCREEN_HEIGHT / TILE_SIZE;
 const GRAVITY = 0.2;
 const JUMP_ACCEL = 8;
+const TIMEMAX = 1000;
+const CLEAR_WAIT = 180;
 
 let canvas;
 let context;
@@ -42,14 +45,16 @@ let pPos = new Vec2(13*TILE_SIZE, 10*TILE_SIZE);
 let pMove = new Vec2(0, 0);
 let pAccel = new Vec2(0, 0);
 let pjumped = false;
+let time;
+let phase;
 let num_present;
 
-let map = [
+let map_ref = [
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -61,6 +66,8 @@ let map = [
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 ];
+
+let map;
 
 
 
@@ -234,8 +241,7 @@ function init() {
 		alert('Web Audio API is not supported in this browser');
 	}
 
-	pPos.x = 0;
-	pPos.y = 10;
+	stageInit();
 
 	Asset.loadAssets(function() {
 		requestAnimationFrame(update);
@@ -246,6 +252,17 @@ function update() {
 	requestAnimationFrame(update);
 	if (Object.keys(gamepads).length > 0) {
 		checkGamepadInput();
+	}
+
+	time++;
+	if (phase == 0 && time >= TIMEMAX) {
+		stageInit();
+	}
+	else if (phase == 1) {
+		if (time >= CLEAR_WAIT) {
+			stageInit();
+		}
+		return;
 	}
 
 	pMove.x = pMove.y = 0;
@@ -265,6 +282,91 @@ function update() {
 	if (Math.abs(pMove.x) > 5) { pMove.x = 5*Math.sign(pMove.x); }
 	if (Math.abs(pMove.y) > 5) { pMove.y = 5*Math.sign(pMove.y); }
 
+
+	checkCollision();
+	pPos = pPos.add(pMove);
+	pPos.x = Math.floor(pPos.x);
+	pPos.y = Math.floor(pPos.y);
+	
+	render();
+}
+
+
+function render() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	context.drawImage(Asset.images['player'], pPos.x, pPos.y, TILE_SIZE, TILE_SIZE);
+
+	num_present = 0;
+	for (let y=0; y<NUM_TILE_Y; y++) {
+		for (let x=0; x<NUM_TILE_X; x++) {
+			if (map[y*NUM_TILE_X+x] == 1) {
+				context.fillStyle = "rgb(255, 0, 255)";
+				context.fillRect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+			}
+			if (map[y*NUM_TILE_X+x] == 2) {
+				num_present++;
+				context.fillStyle = "rgb(0, 255, 255)";
+				context.fillRect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+			}
+		}
+	}
+
+	if (phase == 0) {
+		context.fillStyle = "rgb(200, 128, 50)";
+		context.fillRect((SCREEN_WIDTH-TIMEBAR_WIDTH)/2, 20, TIMEBAR_WIDTH, 10);
+
+		context.fillStyle = "rgb(0, 128, 50)";
+		context.beginPath();
+		context.arc((SCREEN_WIDTH-TIMEBAR_WIDTH)/2 + TIMEBAR_WIDTH * time/TIMEMAX, 25, 12, 0, 2*Math.PI, true);
+		context.fill();
+	}
+
+	document.getElementById("present_counter").innerHTML = "現在のプレゼントは" + num_present + "個";
+	document.getElementById("pPosBox").innerHTML = "(" + pPos.x+ ", " + pPos.y + "), time =" + time;
+}
+
+function checkGamepadInput() {
+	let pads = navigator.getGamepads ? navigator.getGamepads() :
+	(navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+	
+	keys["Up"] = (pads[0].axes[1] < -0.25 ? keys["Up"]+1 : 0);
+	keys["Down"] = (pads[0].axes[1] > 0.25) ? keys["Down"]+1 : 0;
+	keys["Left"] = (pads[0].axes[0] < -0.25 ? keys["Left"]+1 : 0);
+	keys["Right"] = (pads[0].axes[0] > 0.25 ? keys["Right"]+1 : 0);
+	keys["Jump"] = pads[0].buttons[0].pressed ? keys["Jump"]+1 : 0;
+}
+function pointInTile(p) {
+	let mapx = Math.floor(p.x / TILE_SIZE);
+	let mapy = Math.floor(p.y / TILE_SIZE);
+	return map[mapy*NUM_TILE_X+mapx];
+}
+
+function getPresent(p) {
+	let mapx = Math.floor(p.x / TILE_SIZE);
+	let mapy = Math.floor(p.y / TILE_SIZE);
+	map[mapy*NUM_TILE_X+mapx] = 0;
+	num_present--;
+
+	if (num_present == 0) {
+		playSound(Asset.sounds['fanfare']);
+		time = 0;
+		phase = 1;
+	}
+	else {
+		playSound(Asset.sounds['get']);
+	}
+}
+
+function stageInit() {
+	pPos.x = 5*TILE_SIZE;
+	pPos.y = 13*TILE_SIZE;
+	time = 0;
+	phase = 0;
+	map = map_ref.slice();
+}
+
+function checkCollision() {
 	let check = 0, count = 0;
 	let point = new Vec2(0, 0);
 
@@ -316,63 +418,4 @@ function update() {
 				pMove.x = Math.floor((pPos.x+pMove.x) / TILE_SIZE + ((check&0x5) != 0?1:0))*TILE_SIZE - pPos.x;
 		}
 	}
-	pPos = pPos.add(pMove);
-	pPos.x = Math.floor(pPos.x);
-	pPos.y = Math.floor(pPos.y);
-	
-	render();
-}
-
-function pointInTile(p) {
-	let mapx = Math.floor(p.x / TILE_SIZE);
-	let mapy = Math.floor(p.y / TILE_SIZE);
-	return map[mapy*NUM_TILE_X+mapx];
-}
-
-function getPresent(p) {
-	let mapx = Math.floor(p.x / TILE_SIZE);
-	let mapy = Math.floor(p.y / TILE_SIZE);
-	map[mapy*NUM_TILE_X+mapx] = 0;
-	num_present--;
-
-	if (num_present == 0) {
-		playSound(Asset.sounds['fanfare']);
-	}
-	else {
-		playSound(Asset.sounds['get']);
-	}
-}
-
-function render() {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-
-	context.drawImage(Asset.images['player'], pPos.x, pPos.y, TILE_SIZE, TILE_SIZE);
-
-	num_present = 0;
-	for (let y=0; y<NUM_TILE_Y; y++) {
-		for (let x=0; x<NUM_TILE_X; x++) {
-			if (map[y*NUM_TILE_X+x] == 1) {
-				context.fillStyle = "rgb(255, 0, 255)";
-				context.fillRect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE);
-			}
-			if (map[y*NUM_TILE_X+x] == 2) {
-				num_present++;
-				context.fillStyle = "rgb(0, 255, 255)";
-				context.fillRect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE);
-			}
-		}
-	}
-	document.getElementById("present_counter").innerHTML = "現在のプレゼントは" + num_present + "個";
-	document.getElementById("pPosBox").innerHTML = "(" + pPos.x+ ", " + pPos.y + ")";
-}
-
-function checkGamepadInput() {
-	let pads = navigator.getGamepads ? navigator.getGamepads() :
-	(navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
-	
-	keys["Up"] = (pads[0].axes[1] < -0.25 ? keys["Up"]+1 : 0);
-	keys["Down"] = (pads[0].axes[1] > 0.25) ? keys["Down"]+1 : 0;
-	keys["Left"] = (pads[0].axes[0] < -0.25 ? keys["Left"]+1 : 0);
-	keys["Right"] = (pads[0].axes[0] > 0.25 ? keys["Right"]+1 : 0);
-	keys["Jump"] = pads[0].buttons[0].pressed ? keys["Jump"]+1 : 0;
 }
